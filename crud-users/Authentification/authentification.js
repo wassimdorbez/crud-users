9260a674f66051d943af5a2e3003f5df36ecc590
wassim dorboz <105280477+wassimdorbez@users.noreply.github.com>
2022-06-22 00:27:54 +0100

    prepare for vacance
    the JWT coming

--- crud-users/Authentification/authentification.js
+++ crud-users/Authentification/authentification.js
@@ -1,42 +1,30 @@
-const express = require('express')
-
-//mongoDB user model
-const User = require('../model/User');
-
-//mongoDB user verification model
-const UserVerification = require('../model/UserVerification')
-
-
-const jwt = require('jsonwebtoken')
-const expressJwt = require('express-jwt')
+//JWT
+const jwt = require('jsonwebtoken');
+const expressJwt = require('express-jwt');
 
 //password handler
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
 const { signupValidation, signinValidation } = require('../validation');
-
 
 //SignUp
 exports.signup = async (req, res) => {
-    //VALIDATE DATA BEFORE CREATIONG USER 
     const { error } = signupValidation(req.body);
     if (error) return res.status(400).json({
         status: "FAILED",
         message: error.details[0].message
     });
 
-    //checking if the user is already exist in the Database
+    //CHECK USERS.CONTROLLERS FOR MORE DETAILS
     const emailExist = await User.findOne({ email: req.body.email })
     if (emailExist)
         return res.status(400).json({
             status: "FAILED",
             message: "Email  exists"
-        })}
+        })};
 
     //Hash passwords
-    const saltRounds = 11
-    const hashPassword = await bcrypt.hash(req.body.password, saltRounds)
-
-
+    const saltRounds = 11 
+    const hashPassword = await bcrypt.hash(req.body.password, saltRounds);
 
     //SignIn
     exports.signin = async (req, res) => {
@@ -48,16 +36,12 @@ exports.signup = async (req, res) => {
             message: error.details[0].message
         });
 
-
-        //PASSWORD IS CORRECT ?!
-        const validPass = await bcrypt.compare(req.body.password, user.password)
-        if (!validPass) return res.status(400).json({
+        //COMPARE THE PASSWORD RES WITH REQ.BODY
+        const isValid = await bcrypt.compare(req.body.password, user.password)
+        if (!isValid) return res.status(400).json({
             status: "FAILED",
             message: 'Invalid Password'
-        })
-
-
-
+        });
 
         //create and assign a token
         const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET)
@@ -66,18 +50,20 @@ exports.signup = async (req, res) => {
         res.cookie('token', token, { expire: new Date() + 1 })
 
         //SEND RESPONSE TO FRONTEND
-        const { _id, name, email } = user
+        const {   Email,
+            FirstName,
+            LastName,
+            Age } = Users
         return res.json({
             status: "SUCCESS",
             message: "Signin Successful",
             token,
-            user: {
-                _id,
-                name,
-                email
-            }
-        })
-
+            Users: {
+                Email,
+                FirstName,
+                LastName,
+                Age
+            }  })
     }
 //SIGN OUT
     exports.signout = (req, res) => {
@@ -88,19 +74,15 @@ exports.signup = async (req, res) => {
         })
     }
 
-
 //VERIFICATION JWT 
 const jwt = require('jsonwebtoken')
-
 module.exports = function(req, res, next) {
     const token = req.header('auth-token')
     if(!token) return res.status(401).send('Access Denied')
-
     try {
         const verified = jwt.verify(token, process.env.TOKEN_SECRET)
         req.user = verified
         next()
     } catch (err) {
         res.status(400).send('Invalid Token')
-    }
-}
+    }}
