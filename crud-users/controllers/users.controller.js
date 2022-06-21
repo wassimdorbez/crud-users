24127a34cba6f1608c5459f74d40bb9455dc5639
wassim dorboz <105280477+wassimdorbez@users.noreply.github.com>
2022-06-21 06:36:29 +0100

    all user function end
    all conditions to add user finished

--- crud-users/controllers/users.controller.js
+++ crud-users/controllers/users.controller.js
@@ -1,16 +1,27 @@
 const Users = require('../models/users.models')
-
+const ValidateUser = require('../validation/Users.validation')
 
 //add user
 const AddUser = async (req, res) => {
-    //res.send('user added')   add = create
+    const { errors, isValid } = ValidateUser(req.body);
     try {
-        await Users.create(req.body)
-        res.status(201).json({ message: 'user added' })
+        if (!isValid) {
+            res.status(404).json(errors);
+        } else {
+            await Users.findOne({ Email: req.body.Email }).then(async (exist) => {
+                if (exist) {
+                    errors.Email = "User Exist";
+                    res.status(404).json(errors);
+                } else {
+                    await Users.create(req.body);
+                    res.status(201).json({ message: "User added" });
+                }
+            });
+        }
+    } catch (error) {
+        console.log(error.message);
     }
-    catch (error) { console.log(error.message) }
-}
-
+};
 
 //find all users
 const FindAllUsers = async (req, res) => {
@@ -34,13 +45,19 @@ const FindSingleUser = async (req, res) => {
 }
 //update user
 const PutUser = async (req, res) => {
+    const { errors, isValid } = ValidateUser(req.body);
+
     //res.send('user puted') put user = findOneAndUpdate
     try {
+        if (!isValid) {
+            res.status(404).json(errors);
+        } else {
+
         const data = await Users.findOneAndUpdate(
             { _id: req.params.id }, req.body, { new: true }
         )
         res.status(201).json(data)
-    }
+    }}
     catch (error) { console.log(error.message) }
 }
 
@@ -48,7 +65,7 @@ const PutUser = async (req, res) => {
 const DeleteUser = async (req, res) => {
     //res.send('user deleted') drop user = findOneAndRemove DeleteOne
     try {
-       await Users.deleteOne({ _id: req.params.id })
+        await Users.deleteOne({ _id: req.params.id })
         res.status(201).json({ message: 'user deleted' })
     }
     catch (error) { console.log(error.message) }
